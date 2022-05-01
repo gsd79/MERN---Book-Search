591f26c679d29148289f192e571eb1c09d7bcb49
Grace <cgracesharpe@gmail.com>
2022-05-01 18:48:09 -0400

    GD: Addressed TODO: Define the necessary Query and Mutation types

--- server/schmeas/typeDefs.js
+++ server/schmeas/typeDefs.js
@@ -1,47 +1,43 @@
-// // TODO:
+const { gql } = require('apollo-server-express');
 
-// Define the necessary Query and Mutation types:
+const typeDefs = gql`
+type User {
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+        thoughts(username: String): [Thought]
+        thought(_id: ID!): Thought
+    }
 
-// Query type:
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: Book!): User
+        removeBook(bookId: ID!): User
+    }
 
-// me: Which returns a User type.
-// Mutation type:
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]        
+    }
 
-// login: Accepts an email and password as parameters; returns an Auth type.
+    type Book {
+        bookId: String
+        authors:[String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
 
-// addUser: Accepts a username, email, and password as parameters; returns an Auth type.
+    type Auth {
+        token: ID!
+        user: User
+    }
+`;
 
-// saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. 
-// (Look into creating what's known as an input type to handle all of these parameters!)
-
-// removeBook: Accepts a book's bookId as a parameter; returns a User type.
-
-// User type:
-
-// _id
-
-// username
-
-// email
-
-// bookCount
-
-// savedBooks (This will be an array of the Book type.)
-
-// Book type:
-
-// bookId (Not the _id, but the book's id value returned from Google's Book API.)
-
-// authors (An array of strings, as there may be more than one author.)
-
-// description
-
-// title
-
-// image
-
-// link
-
-// Auth type:
-
-//  * token
+module.exports = typeDefs;
